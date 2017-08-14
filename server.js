a446f64e0cd5efcd397140160d83b73ef79838e0
allumonica <30749480+allumonica@users.noreply.github.com>
2017-08-14 21:03:20 +0800

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -110,7 +110,7 @@ app.get('/ui/madi.png', function (req, res) {
 var names = [];
 app.get('/submit-name/:name' , function(req, res) {
     //Get the name from the request
-    var names = req.params.name;
+    var name = req.params.name;
     
     
     names.push(name);
