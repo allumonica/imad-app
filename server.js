f2b453f77886a594e4e92a0f42c7d4eb5db006a6
allumonica <30749480+allumonica@users.noreply.github.com>
2017-08-21 18:22:29 +0800

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -1,7 +1,15 @@
 var express = require('express');
 var morgan = require('morgan');
 var path = require('path');
+var pool = require('pg').pool;
 
+var config = {
+    user: 'allumonica2012',
+    database: 'allumonica2012',
+    host: 'db.imad.hasura-app.io',
+    port: '5432', 
+    password: process.env.DB_PASSWORD
+}
 var app = express();
 app.use(morgan('combined'));
 
@@ -78,6 +86,20 @@ function CreateTemplate (data) {
          
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
+});
+
+var pool = new pool(config);
+app.get('/test-db', function (req, res) {
+    //make a select request
+    //return a response with the results
+    pool.query('SELECT * FROM test', function (err , results) {
+        if (err) {
+         res.status(500).send(err.toString());
+        } else {
+            res.send(JSON.stringify(result));
+        
+        }
+    });
 });
 
 var counter = 0;
