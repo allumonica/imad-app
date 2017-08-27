2b44140e2f587e58a163e27ff11d81e04b30424d
allumonica <30749480+allumonica@users.noreply.github.com>
2017-08-27 22:41:47 +0800

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -3,6 +3,8 @@ var morgan = require('morgan');
 var path = require('path');
 var Pool = require('pg').Pool;
 var crypto = require('crypto');
+var bodyParser = require('body-parser');
+
 
 var config = {
     user: 'allumonica2012',
@@ -14,7 +16,7 @@ var config = {
 
 var app = express();
 app.use(morgan('combined'));
-
+app.use(bodyParser.json());
 
 var articles = {
     'article-one': {
@@ -95,17 +97,30 @@ function hash(input, salt){
     //How do we create a hash?
     var hashed = crypto.pbkdf2Sync(input, salt, 10000, 512, 'sha512');
     return ["pbkdf2", "10000", salt,  hashed.toString('hex')].join('$'); 
-    
-    
 }
-
-
 
 app.get('/hash/:input', function(req, res) {
   var hashedString = hash(req.params.input, 'this-is-some-random-string');
   res.send(hashedString);
+});
   
+app.post('/create-user', function(req, res) {
+    //username, password
+    //{"username": "allumonica", "password": "password"
+    //JSON
+    var username = req.body.username;
+    var password = req.body.password;
+var salt = crypto.getRandonBytes(128).toString('hex');
+var dbString = hash(password, salt);
+pool.query('INSERT INTO "user" (username, password) VALUES ($1, $2)', [username, dbString], function(err, result) {
+          if (err) {
+         res.status(500).send(err.toString());
+        } else {
+            res.send('user successfully created:' + username);
+        } 
+
     
+});
     
 });
 
