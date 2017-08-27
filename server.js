d9673c28dc630a5a065eb46da6f1ea80d3314a5f
allumonica <30749480+allumonica@users.noreply.github.com>
2017-08-27 21:21:27 +0800

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -129,7 +129,7 @@ app.get('/articles/:articleName',function (req, res) {
           res.status(500).send(err.toString());
       } else {
           if (result.rows.length === 0) {
-          result.status(404).send('Article not found');
+          res.status(404).send('Article not found');
       } else {
           var articleData = result.rows[0];
         res.send(CreateTemplate(articlesData)); 
