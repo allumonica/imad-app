48ff35bf449c883b3f7f0c3fc062fb4a0312782c
allumonica <30749480+allumonica@users.noreply.github.com>
2017-09-06 20:43:17 +0800

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ var config = {
     user: 'allumonica2012',
     database: 'allumonica2012',
     host: 'db.imad.hasura-app.io',
-    port: '5432', 
+    port: 5432, 
     password: process.env.DB_PASSWORD
 };
 
