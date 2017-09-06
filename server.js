4e36516c312cc104e04f1a35e6a8d1177d77e12d
allumonica <30749480+allumonica@users.noreply.github.com>
2017-09-06 19:34:26 +0800

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -106,7 +106,7 @@ app.get('/hash/:input', function(req, res) {
   
 app.post('/create-user', function(req, res) {
     //username, password
-    //{"username": "allumonica", "password": "password"
+    //{"username": "monica", "password": "password"
     //JSON
     var username = req.body.username;
     var password = req.body.password;
