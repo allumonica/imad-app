838ada10e427b36a9c38bc0b5c6e5067f58dd4fc
allumonica <30749480+allumonica@users.noreply.github.com>
2017-09-07 18:42:47 +0800

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -12,13 +12,13 @@ submit.onclick = function() {
          //Take some action
        if (request.status === 200) {
         //Capture a list of names and render it as a list
-               Console.log('user logged in');
+               console.log('user logged in');
                alert('Logged in successfully');
          } else if (request.status === 403) {
              alert('Username/password is incorrect');
          } else if (request.status === 500) {
              alert('Something went wrong on the server');
-         }   
+         }
      }
       //Not done yet
   };
@@ -26,8 +26,8 @@ submit.onclick = function() {
   //Make the request
  var username = document.getElementById('username').value;
  var password = document.getElementById('password').value;
- Console.log(username);
- Console.log(password);
+ console.log(username);
+ console.log(password);
  request.open('POST' , 'http://allumonica2012.imad.hasura-app.io/login', true);
  request.setRequestHeader('Content.Type','application/json');
  request.send(JSON.stringify({username: username, password: password}));
